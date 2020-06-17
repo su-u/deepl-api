7cbb5f42b08a1c5bb10d2a8353ece348e390b633
su- <aaaiiiuuu35@gmail.com>
2020-06-17 19:58:18 +0900

    Feat: 型

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,36 @@ import axios from 'axios';
 import { stringify } from 'query-string';
 
 type LangDict =
-    | 'DE'
-    | 'EN'
-    | 'FR'
-    | 'IT'
-    | 'JA'
-    | 'ES'
-    | 'NL'
-    | 'PL'
-    | 'PT'
-    | 'RU'
-    | 'ZH';
+  | 'DE'
+  | 'EN'
+  | 'FR'
+  | 'IT'
+  | 'JA'
+  | 'ES'
+  | 'NL'
+  | 'PL'
+  | 'PT'
+  | 'RU'
+  | 'ZH';
 
-type DeeplOptionalParameters = {
-    split_sentences: any;
-    source_lang: LangDict;
-    preserve_formatting: '0' | '1';
-    formality: '0' | '1';
+interface DeeplOptionalParameters {
+  split_sentences: any;
+  source_lang: LangDict;
+  preserve_formatting: '0' | '1';
+  formality: '0' | '1';
+}
+
+interface DeeplParameters extends Partial<DeeplOptionalParameters> {
+  text: string | string[];
+  target_lang: LangDict;
+  auth_key: string;
+}
+
+const ENDPOINT = 'https://api.deepl.com/v2/translate';
+
+export const translate = (parameters: DeeplParameters) => {
+  return axios.post(
+    ENDPOINT,
+    stringify(parameters)
+  );
 };
-
-type DeeplParameters = {
-    text: string | string[];
-    target_lang: LangDict;
-    auth_key: string;
-} & Partial<DeeplOptionalParameters>;
-
-const translate = (parameters: DeeplParameters) => {
-    return axios.post(
-        'https://api.deepl.com/v2/translate',
-        stringify(parameters)
-    );
-};
-
-translate({
-    text: ['おはようございます', 'こんばんは'],
-    source_lang: 'JA',
-    target_lang: 'EN',
-    auth_key: 'bc0db0be-899a-e6f6-7b29-29f0779546b7',
-})
-    .then(result => {
-        console.log(result.data);
-    })
-    .catch(error => {
-        console.error(error);
-    });
