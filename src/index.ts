cad435bf0fadf1c8eaa436aa3fe9060bf51a1c66
su- <aaaiiiuuu35@gmail.com>
2020-06-19 16:55:17 +0900

    Feat: index変更

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import axios from 'axios';
-import { stringify } from 'query-string';
-import { DeeplApiParameters, Response } from "./interface";
-
-const ENDPOINT = 'https://api.deepl.com/v2/translate';
-
-export const translate = (parameters: DeeplApiParameters) => {
-  return axios.post<Readonly<Response>>(
-    ENDPOINT,
-    stringify(parameters)
-  );
-};
-
-export default translate;
+export * from './deepl';
+export * from './interface';
+export * from './language';
